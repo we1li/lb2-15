3d389197040be8d2f773d66c18ac57190794c9db
we1li <125406661+we1li@users.noreply.github.com>
2023-10-02 09:03:06 +0300

    individual3.py

--- prog/individual3.py
+++ prog/individual3.py
@@ -4,11 +4,12 @@
 import os
 from datetime import datetime
 
+
 def display_file_info(folder_path):
     # Получаем список файлов в папке
     files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
-    print("File Information:")
+    print("Информация о файлах:")
     for file in files:
         file_path = os.path.join(folder_path, file)
 
@@ -22,12 +23,11 @@ def display_file_info(folder_path):
         # Выводим информацию о файле
         print(file_info)
 
+
 if __name__ == "__main__":
-    folder_path = './files'  # Путь к папке с файлами
+    folder_path = './индивдульное 3'  # Путь к папке с файлами
 
     try:
         display_file_info(folder_path)
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-
-        
